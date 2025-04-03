610a8e97b7652f266ffd4c0656dd4688cfb2676d
Christian Landgren <christian@landgren.nu>
2025-04-03 15:47:39 +0200

    fix: remove unused files

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Replace environment variables in the nginx configuration
-envsubst '${API_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-# Execute the main container command
-exec "$@"
